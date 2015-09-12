b33a0d0c42098fadf8503cd3b578b210c9216d22
Jef Claes <jef.claes@gmail.com>
2015-09-12 23:07:20 +0200

    Added scenario

--- src/StateWithEvents/StateWithEvents/Program.cs
+++ src/StateWithEvents/StateWithEvents/Program.cs
@@ -8,12 +8,24 @@ namespace StateWithEvents
     {
         public static void Main(string[] args)
         {
-            var player = Player.Register();
+            var player = Player.Register("1", "jef");
 
             player.Lock();
 
-            Console.WriteLine(string.Format("State = {0}", player.State()));
-            Console.WriteLine(string.Format("Events = {0}", string.Join(", ", player.Events().Select(e => e.GetType().Name))));
+            var state = player.State();
+            var events = player.Events();
+
+            Console.WriteLine(string.Format("State = {0}", state));
+            Console.WriteLine(string.Format("Events = {0}", string.Join(", ", events.Select(e => e.GetType().Name))));
+
+            new Scenario<Player, PlayerState>()
+                .Given(() => Player.Register("1", "jef"))
+                .When(sut => sut.Lock())
+                .ThenState(new PlayerState()
+                    { Id = "1", Locked = true, Nickname = "jef" })
+                .ThenEvent(new PlayerRegistered("1", "jef"))
+                .ThenEvent(new PlayerLocked("1"))
+                .Run();
 
             Console.ReadLine();
         }
@@ -29,15 +41,64 @@ namespace StateWithEvents
         IEnumerable<object> Events();
     }
 
+    public class Scenario<TAggregate, TState> where TAggregate : IStatefulAggregate<TState>, IEventRecordingAggregate
+    {
+        private Func<TAggregate> _factory;
+        private Action<TAggregate> _when;
+        private object _expectedState;
+        private List<object> _expectedEvents = new List<object>();
+
+        public Scenario<TAggregate, TState> Given(Func<TAggregate> factory)
+        {
+            _factory = factory;
+
+            return this;
+        }
+
+        public Scenario<TAggregate, TState> When(Action<TAggregate> when)
+        {
+            _when = when;
+
+            return this;
+        }
+
+        public Scenario<TAggregate, TState> ThenState(object state)
+        {
+            _expectedState = state;
+
+            return this;
+        }
+
+        public Scenario<TAggregate, TState> ThenEvent(object @event) 
+        {
+            _expectedEvents.Add(@event);
+
+            return this;
+        }
+
+        public void Run()
+        {
+            var sut = _factory();
+
+            _when(sut);
+
+            var state = sut.State();
+            var events = sut.Events();
+
+            if (state.Equals(_expectedState)) throw new InvalidOperationException("State assertion failed");
+            if (events.Count() != _expectedEvents.Count()) throw new InvalidOperationException("Events assertion failed");
+        }
+    }
+
     public class Player : IStatefulAggregate<PlayerState>, IEventRecordingAggregate
     {
         private readonly PlayerState _state;
         private readonly List<object> _events = new List<object>();        
 
-        private Player(Guid id)
+        private Player(string id, string nickname)
         {
             _state = new PlayerState();
-            Apply(new PlayerRegistered(id));
+            Apply(new PlayerRegistered(id, nickname));
         }
 
         public Player(PlayerState state)
@@ -59,6 +120,7 @@ namespace StateWithEvents
         private void When(PlayerRegistered e)
         {
             _state.Id = e.PlayerId;
+            _state.Nickname = e.Nickname;
         }
 
         public PlayerState State()
@@ -77,41 +139,61 @@ namespace StateWithEvents
             return _events;
         }
 
-        public static Player Register()
+        public static Player Register(string id, string nickname)
         {
-            return new Player(Guid.NewGuid());
+            return new Player(id, nickname);
         }
     }
 
     public class PlayerState
     {
-        public Guid Id { get; set; }
+        public string Id { get; set; }
+
+        public string Nickname { get; set; }
 
         public bool Locked { get; set; }
 
         public override string ToString()
         {
-            return string.Format("Id = {0} Locked = {1}", Id, Locked);
+            return string.Format("Id = {0} Nickname = {1} Locked = {2}", Id, Nickname, Locked);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj != null) return false;
+            if (obj.GetType() != typeof(PlayerState)) return false;
+
+            var other = (PlayerState)obj;
+
+            return other.Id == Id && other.Nickname == Nickname && other.Locked == Locked;
+        }
+
+        public override int GetHashCode()
+        {
+            return 1;            
         }
     }
 
     public class PlayerRegistered
     {
-        public PlayerRegistered(Guid playerId)
+        public PlayerRegistered(string playerId, string nickname)
         {
             PlayerId = playerId;
+            Nickname = nickname;
         }
 
-        public Guid PlayerId { get; set; }
+        public string PlayerId { get; set; }
+
+        public string Nickname { get; set; }
     }
 
     public class PlayerLocked
     {
-        public PlayerLocked(Guid playerId)
+        public PlayerLocked(string playerId)
         {
             PlayerId = playerId;
         }
 
-        public Guid PlayerId { get; set; }                
-    }
+        public string PlayerId { get; set; }                
+    }  
 }
